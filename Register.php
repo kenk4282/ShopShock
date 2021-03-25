973bddd63ae69a3978ed327a5c6b013cd21d633e
kenk4282 <6306021422186@fitm.kmutnb.ac.th>
2021-03-25 15:15:23 +0700

    update

--- Register.php
+++ Register.php
@@ -6,18 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
 </head>
-<body>
+<body style="margin: 0 auto; width:50%">
     <center>
     <h1>ShopShock Member Register</h1>
     <form action="handle.php" method="post">
+    <div style="border: red 1px solid;padding:20px">
     <table>
         <th>
             <td colspan="2" align="right"><a href="login.php">login</a></td>
         </th>
         <tr>
-            <td colspan="2">_________________________________________</td>
+            <td colspan="2"><hr></td>
         </tr>
-        <tr><td><br></td></tr>
+        <tr><td></td></tr>
         <tr>
             <td>Name :</td><td><input type="text" name="insert_name" id="" required></td>
         </tr>
@@ -30,10 +31,14 @@
         <tr>
             <td>Confirm Password :</td><td><input type="password" name="insert_compass" id="" required></td>
         </tr>
+        <tr><td></td></tr>
+        <tr>
+            <td colspan="2"><hr></td>
+        </tr>
     </table><br>
     <button type="submit">Submit</button><button type="reset">Reset</button>
+    </div>
     </form>
-    _________________________________________
     </center>
     
 </body>
